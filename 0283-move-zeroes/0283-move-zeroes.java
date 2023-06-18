c8565a59751ae28bddd5a5378d2353d84303d568
Nausheen Anwar <80268496+9sheen-Anwar@users.noreply.github.com>
2023-06-18 14:53:37 +0530

    Time: 1 ms (100%), Space: 45.5 MB (10.59%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j=0,count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0)
+                count++;
+            else{
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        for(int i=j;i<nums.length;i++){
+            nums[i]=0;
+        }
+        
+    }
+}
